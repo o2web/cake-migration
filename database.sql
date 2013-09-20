de5002d5286c05f4f40a6671861236bef1d09d9d
kgiguere <kgiguere@9d2c35e4-253f-4dd7-b3e9-596db99104a9>
2013-09-20 21:42:48 +0000

    sql tables
    git-svn-id: svn+ssh://208.111.94.3/svn/plugins/migration/trunk/migration@300 9d2c35e4-253f-4dd7-b3e9-596db99104a9

---
+++ database.sql
@@ -0,0 +1,37 @@
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migration_nodes`
+--
+
+CREATE TABLE IF NOT EXISTS `migration_nodes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `local_id` int(11) DEFAULT NULL,
+  `tracked` tinyint(1) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `migration_remotes`
+--
+
+CREATE TABLE IF NOT EXISTS `migration_remotes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `model` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `instance` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `local_id` int(11) DEFAULT NULL,
+  `remote_id` int(11) DEFAULT NULL,
+  `sign` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `overridable` text COLLATE utf8_unicode_ci,
+  `updated` datetime DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
